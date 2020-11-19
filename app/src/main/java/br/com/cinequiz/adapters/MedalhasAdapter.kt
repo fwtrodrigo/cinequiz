b8707b184a08749caada20b9ced45d7694cc36d5
Kaio Donadelli <xhiko_canhoto@yahoo.com.br>
2020-11-19 18:31:07 -0300

    Ajustes de layout, testes de ícones. Falta definir dinamicamente o ícone com base no ID da medalha.

--- app/src/main/java/br/com/cinequiz/adapters/MedalhasAdapter.kt
+++ app/src/main/java/br/com/cinequiz/adapters/MedalhasAdapter.kt
@@ -1,14 +1,20 @@
 package br.com.cinequiz.adapters
 
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import br.com.cinequiz.R
 import br.com.cinequiz.domain.Medalha
 
-class MedalhasAdapter (private val listaMedalhas: ArrayList<Medalha>): RecyclerView.Adapter<MedalhasAdapter.MedalhaViewHolder>() {
+class MedalhasAdapter (private val listaMedalhas: ArrayList<Medalha>, val context: Context): RecyclerView.Adapter<MedalhasAdapter.MedalhaViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MedalhasAdapter.MedalhaViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_medalha, parent, false)
         return MedalhaViewHolder(itemView)
@@ -18,6 +24,9 @@ class MedalhasAdapter (private val listaMedalhas: ArrayList<Medalha>): RecyclerV
         var medalha = listaMedalhas[position]
         holder.tvMedalhaTit.text = medalha.tit
         holder.tvMedalhaDesc.text = medalha.desc
+//        holder.ivMedalhaIcone.setImageDrawable()
+
+
     }
 
     override fun getItemCount() = listaMedalhas.size
@@ -25,5 +34,6 @@ class MedalhasAdapter (private val listaMedalhas: ArrayList<Medalha>): RecyclerV
     class MedalhaViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         var tvMedalhaTit: TextView = itemView.findViewById(R.id.tvMedalhaTit)
         var tvMedalhaDesc: TextView = itemView.findViewById(R.id.tvMedalhaDesc)
+        var ivMedalhaIcone: ImageView = itemView.findViewById(R.id.ivMedalhaIcone)
     }
 }
