acd82c30283aaa50e99571c2fe78c5c34fb32fe6
AlineCruzDEV <linegcruz@gmail.com>
2020-11-18 22:42:02 -0300

    Splash Screen utilizando Coroutines

--- app/src/main/java/br/com/cinequiz/SplashScreen.kt
+++ app/src/main/java/br/com/cinequiz/SplashScreen.kt
@@ -1,11 +1,27 @@
 package br.com.cinequiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        scope.launch {
+            delay(3000)
+            val intent = Intent(application, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
