3578dbe66e2c91b2d8f3c5b58829f682150682ad
Mateus <mateustrindade8@gmail.com>
2020-11-21 20:49:38 -0300

    Inclusão do JogoDica.kt + implementação da toolbar nas MedalhasActivity.kt

--- app/src/main/java/br/com/cinequiz/MedalhasActivity.kt
+++ app/src/main/java/br/com/cinequiz/MedalhasActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.viewModels
 import br.com.cinequiz.adapters.MedalhasAdapter
 import br.com.cinequiz.domain.Medalha
 import br.com.cinequiz.models.MedalhaViewModel
+import kotlinx.android.synthetic.main.activity_cadastro.*
 import kotlinx.android.synthetic.main.activity_medalhas.*
+import kotlinx.android.synthetic.main.toolbar_main.view.*
 
 class MedalhasActivity : AppCompatActivity() {
 
@@ -15,6 +17,10 @@ class MedalhasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medalhas)
+
+        toolbarMedalhas.toolbarMain.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         val listaMedalhas = viewModel.getListMedalhasGson()
         val adapter = MedalhasAdapter(listaMedalhas)
