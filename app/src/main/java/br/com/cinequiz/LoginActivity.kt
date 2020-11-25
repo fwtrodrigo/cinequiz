3cf89aabb21f363fa9f0e29134bd32bf27ac1659
Rodrigo Freitas <fwtcursos@gmail.com>
2020-11-25 18:51:39 -0300

    mudar cor do edittext ao receber foco

--- app/src/main/java/br/com/cinequiz/LoginActivity.kt
+++ app/src/main/java/br/com/cinequiz/LoginActivity.kt
@@ -1,11 +1,15 @@
 package br.com.cinequiz
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,10 @@ class LoginActivity : AppCompatActivity() {
 
         edtLoginSenha.setOnFocusChangeListener { view, hasFocus ->
             if(hasFocus) {
-                view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel_selecionado)
+                view.background =  ContextCompat.getDrawable(
+                    this,
+                    R.drawable.shape_anel_selecionado
+                )
             } else {
                 view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel)
             }
@@ -37,5 +44,7 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, MenuActivity::class.java))
             finish()
         }
+
+
     }
 }
