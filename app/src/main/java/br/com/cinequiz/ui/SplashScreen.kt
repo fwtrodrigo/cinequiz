d63daa1c16b095c7c480fde7c74ee989ef6e07da
Mateus <mateustrindade8@gmail.com>
2020-12-23 21:30:14 -0300

    Inclusão de login via Google e Facebook com Firebase

--- app/src/main/java/br/com/cinequiz/ui/SplashScreen.kt
+++ app/src/main/java/br/com/cinequiz/ui/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.cinequiz.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -12,17 +13,29 @@ import kotlinx.coroutines.launch
 class SplashScreen : AppCompatActivity() {
 
     val scope = CoroutineScope(Dispatchers.Main)
+    private lateinit var  mAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
         scope.launch {
             delay(3000)
-            val intent = Intent(application, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            if(user != null){
+                val intent = Intent(application, MenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                val intent = Intent(application, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
-
     }
 }
+
+
