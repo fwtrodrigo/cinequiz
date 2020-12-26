2cfe4aba987b26d47e2f1c8cc2bcedb4789ce5ab
Rodrigo Gonçalves de Freitas <rodrigo.freitas@finnet.corp>
2020-12-25 23:04:11 -0300

    Adicao cadastro por email, e logar com email

--- app/src/main/java/br/com/cinequiz/ui/CadastroActivity.kt
+++ app/src/main/java/br/com/cinequiz/ui/CadastroActivity.kt
@@ -1,66 +1,158 @@
 package br.com.cinequiz.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import br.com.cinequiz.R
-import kotlinx.android.synthetic.main.activity_cadastro.*
-import kotlinx.android.synthetic.main.toolbar_main.view.*
+import br.com.cinequiz.databinding.ActivityCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class CadastroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        toolbarCadastro.toolbarMain.setNavigationOnClickListener {
+        binding.toolbarCadastro.toolbarMain.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        btnCadastroCadastrar.setOnClickListener {
-            Toast.makeText(this, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show()
-            finish()
-        }
+        binding.btnCadastroCadastrar.setOnClickListener {
+            val nome = binding.edtCadastroNome.text.toString()
+            val email = binding.edtCadastroEmail.text.toString()
+            val senha = binding.edtCadastroSenha.text.toString()
+            val confirmacao = binding.edtCadastroConfirmacaoSenha.text.toString()
 
-        edtCadastroNome.setOnFocusChangeListener { view, hasFocus ->
-            if(hasFocus) {
-                view.background =  ContextCompat.getDrawable(this,
-                    R.drawable.shape_anel_selecionado
-                )
-            } else {
-                view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel)
+            if (validaFormulario(nome, email, senha, confirmacao)) {
+                criaUsuario(email, senha)
             }
         }
 
-        edtCadastroEmail.setOnFocusChangeListener { view, hasFocus ->
-            if(hasFocus) {
-                view.background =  ContextCompat.getDrawable(this,
+        binding.edtCadastroNome.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(
+                    this,
                     R.drawable.shape_anel_selecionado
                 )
             } else {
-                view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel)
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
             }
         }
 
-        edtCadastroSenha.setOnFocusChangeListener { view, hasFocus ->
-            if(hasFocus) {
-                view.background =  ContextCompat.getDrawable(this,
+        binding.edtCadastroEmail.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(
+                    this,
                     R.drawable.shape_anel_selecionado
                 )
             } else {
-                view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel)
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
             }
         }
 
-        edtCadastroConfirmacaoSenha.setOnFocusChangeListener { view, hasFocus ->
-            if(hasFocus) {
-                view.background =  ContextCompat.getDrawable(this,
+        binding.edtCadastroSenha.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(
+                    this,
                     R.drawable.shape_anel_selecionado
                 )
             } else {
-                view.background =  ContextCompat.getDrawable(this, R.drawable.shape_anel)
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
             }
         }
 
+        binding.edtCadastroConfirmacaoSenha.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(
+                    this,
+                    R.drawable.shape_anel_selecionado
+                )
+            } else {
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
+            }
+        }
+    }
+
+    private fun criaUsuario(email: String, senha: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, senha)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast("Conta criada com sucesso!")
+                    voltaPaginaLogin()
+                }
+            }.addOnFailureListener {
+                showToast(
+                    "Erro ao cadastrar usuário. ${
+                        it.message
+                    }"
+                )
+            }
+    }
+
+    private fun validaFormulario(
+        nome: String,
+        email: String,
+        senha: String,
+        confirmacao: String
+    ): Boolean {
+        return when {
+            nome.isEmpty() -> {
+                binding.edtCadastroNome.requestFocus()
+                binding.edtCadastroNome.error = "Obrigatório"
+                showToast("Favor, preencher o campo de nome")
+                false
+            }
+
+            email.isEmpty() -> {
+                binding.edtCadastroEmail.requestFocus()
+                binding.edtCadastroEmail.error = "Obrigatório"
+                showToast("Favor, preencher o campo de e-mail")
+                false
+            }
+
+            senha.isEmpty() -> {
+                binding.edtCadastroSenha.requestFocus()
+                binding.edtCadastroSenha.error = "Obrigatório"
+                showToast("Favor, preencher o campo de senha")
+                false
+            }
+
+            confirmacao.isEmpty() -> {
+                binding.edtCadastroConfirmacaoSenha.requestFocus()
+                binding.edtCadastroConfirmacaoSenha.error = "Obrigatório"
+                showToast("Favor, preencher o campo de confirmação de senha")
+                false
+            }
+
+            senha != confirmacao -> {
+                showToast("A senha e a confirmação não são iguais")
+                false
+            }
+            else -> {
+                binding.edtCadastroNome.error = null
+                binding.edtCadastroEmail.error = null
+                binding.edtCadastroSenha.error = null
+                binding.edtCadastroConfirmacaoSenha.error = null
+                true
+            }
+        }
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun voltaPaginaLogin() {
+        finish()
     }
 }
+
--- app/src/main/java/br/com/cinequiz/ui/LoginActivity.kt
+++ app/src/main/java/br/com/cinequiz/ui/LoginActivity.kt
@@ -7,135 +7,65 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import br.com.cinequiz.R
+import br.com.cinequiz.databinding.ActivityLoginBinding
 import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
-import com.facebook.appevents.AppEventsLogger
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
-import com.facebook.login.widget.LoginButton
-import com.google.firebase.auth.FacebookAuthProvider
 
 //851294246873-0gel4t589odrfmqa2r5vl0si8n7i68f7.apps.googleusercontent.com
 
 class LoginActivity : AppCompatActivity() {
     private val TAG = "LoginActivity"
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
     private var RC_SIGN_IN = 100
-    //private lateinit var facebookSignInBtn: LoginButton
+    private lateinit var binding: ActivityLoginBinding
+
     private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        controlaFocoEditText()
 
         mAuth = FirebaseAuth.getInstance()
 
-        //Facebook Auth
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-
-        //facebookSignInBtn = findViewById(R.id.facebookSignInButton)
-
         callbackManager = CallbackManager.Factory.create()
 
-        facebookSignInButton.setOnClickListener{
-            LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile", "email", "user_friends"))
+        binding.facebookSignInButton.setOnClickListener {
+            callFacebookRegisterCallback()
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this, listOf("public_profile", "email", "user_friends"))
         }
-        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-            override fun onSuccess(loginResult: LoginResult?) {
-                Log.d("TAG", "Success Login")
-                //handleFacebookAccessToken(loginResult.accessToken)
-                // Get User's Info
-            }
 
-            override fun onCancel() {
-                Toast.makeText(this@LoginActivity, "Login Cancelled", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onError(exception: FacebookException) {
-                Toast.makeText(this@LoginActivity, "exception.message", Toast.LENGTH_LONG).show()
-            }
-        })
-        //facebookSignInBtn.setReadPermissions("email", "public_profile")
-
-//        facebookSignInBtn.registerCallback(callbackManager, object :
-//            FacebookCallback<LoginResult> {
-//            override fun onSuccess(loginResult: LoginResult) {
-//                Log.d(TAG, "facebook:onSuccess:$loginResult")
-//                handleFacebookAccessToken(loginResult.accessToken)
-//            }
-//
-//            override fun onCancel() {
-//                Log.d(TAG, "facebook:onCancel")
-//            }
-//
-//            override fun onError(error: FacebookException) {
-//                Log.d(TAG, "facebook:onError", error)
-//            }
-//        })
-
-        //Google Auth
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        googleSignInButton.setOnClickListener() {
+        binding.googleSignInButton.setOnClickListener() {
             googleSignIn()
         }
 
-
-        edtLoginEmail.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel_selecionado)
-            } else {
-                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
-            }
-        }
-
-        edtLoginSenha.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                view.background = ContextCompat.getDrawable(
-                    this,
-                    R.drawable.shape_anel_selecionado
-                )
-            } else {
-                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
-            }
-        }
-
-
-        btnLoginCadastrar.setOnClickListener {
+        binding.btnLoginCadastrar.setOnClickListener {
             startActivity(Intent(this, CadastroActivity::class.java))
         }
 
-        btnLoginEntrar.setOnClickListener {
-            appLogin()
+        binding.btnLoginEntrar.setOnClickListener {
+            signinWithEmail()
         }
 
 
     }
 
-    fun googleSignIn() {
-        val signInIntent: Intent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+
+
+        showToast("onActivityResult:   " + data.toString())
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
 //        if (requestCode == RC_SIGN_IN) {
@@ -151,6 +81,18 @@ class LoginActivity : AppCompatActivity() {
 //                // ...
 //            }
 //        }
+    }
+
+    private fun googleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
@@ -185,9 +127,83 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-
-    fun appLogin() {
+    private fun appLogin() {
         startActivity(Intent(this, MenuActivity::class.java))
         finish()
+    }
+
+    private fun signinWithEmail() {
+        val email = binding.edtLoginEmail.text.toString()
+        val senha = binding.edtLoginSenha.text.toString()
+
+        if (!validaFormulario(email, senha)) {
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast("Bem vindo, UserID: ${it.result?.user?.uid}")
+                    appLogin()
+                }
+            }.addOnFailureListener {
+                showToast(it.message.toString())
+            }
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun validaFormulario(email: String, senha: String): Boolean {
+        return if (email.isEmpty() || senha.isEmpty()) {
+            showToast("Email e senha devem ser informados")
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun callFacebookRegisterCallback() {
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    Log.d("TAG", "Success Login")
+                }
+
+                override fun onCancel() {
+                    Toast.makeText(this@LoginActivity, "Login Cancelled", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: FacebookException) {
+                    Toast.makeText(this@LoginActivity, "exception.message", Toast.LENGTH_LONG)
+                        .show()
+                }
+            })
+    }
+
+    private fun controlaFocoEditText() {
+        binding.edtLoginEmail.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel_selecionado)
+            } else {
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
+            }
+        }
+
+        binding.edtLoginSenha.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.background = ContextCompat.getDrawable(
+                    this,
+                    R.drawable.shape_anel_selecionado
+                )
+            } else {
+                view.background = ContextCompat.getDrawable(this, R.drawable.shape_anel)
+            }
+        }
     }
 }
